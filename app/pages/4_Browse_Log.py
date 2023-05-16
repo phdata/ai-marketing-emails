27325cb95b9ae370cd0519542fe3b72bcb53429b
Peter Hansen <phansen@phdata.io>
2023-05-16 09:52:46 -0500

    Add logging

---
+++ app/pages/4_Browse_Log.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.markdown(open("app/log.md").read())
--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,12 @@
 import openai
 import streamlit as st
 import os
+import datetime
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-def submit_prompt(system_prompt, user_prompt):
+def submit_prompt(system_prompt, user_prompt, log=True):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -19,7 +20,15 @@ def submit_prompt(system_prompt, user_prompt):
 
     # print out partial progress
     for chunk in response:
-        chunk_message = chunk["choices"][0]["delta"]  # extract the message
+        chunk_message = chunk["choices"][0]["delta"]
         collected_messages.append(chunk_message)
         full_reply_content = "".join([m.get("content", "") for m in collected_messages])
         response_container.write(full_reply_content)
+
+    with open("app/log.md", "a") as f:
+        f.write(f"# {datetime.datetime.now()}\n")
+        prompt_markdown = "  \n".join(system_prompt.split("\n"))
+        f.write(f"## System Prompt\n{prompt_markdown}\n")
+        prompt_markdown = "  \n".join(user_prompt.split("\n"))
+        f.write(f"## User Prompt\n{prompt_markdown}\n")
+        f.write(f"## Reply\n{full_reply_content}\n")
